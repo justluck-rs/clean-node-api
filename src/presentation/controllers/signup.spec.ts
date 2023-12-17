7e43d4c08464b858412bab28560bab1e4a861733
justluck-rs <luckcastle@outlook.com>
2023-12-17 17:21:54 -0300

    feat: add all files initial commit

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,33 @@
+import { SignUpController } from "./signup"
+
+describe("SignUp Controller", () => {
+  test("Should return 400 if no name is provided", () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    }
+    const httpRespone = sut.handle(httpRequest)
+    expect(httpRespone.statusCode).toBe(400)
+    expect(httpRespone.body).toEqual(new Error("Missing param: name"))
+  })
+  test("Should return 400 if no email is provided", () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    }
+    const httpRespone = sut.handle(httpRequest)
+    expect(httpRespone.statusCode).toBe(400)
+    expect(httpRespone.body).toEqual(new Error("Missing param: email"))
+  })
+})
+
+
+
