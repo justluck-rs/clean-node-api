46903abc4cce246462436d7245b3fffbbf8bb29f
justluck-rs <luckcastle@outlook.com>
2023-12-17 21:38:07 -0300

    refactor: simplify test generic code

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,30 +2,29 @@ import { MissingParamError } from "../erros/missing-param-error";
 import { SignUpController } from "./signup";
 
 describe("SignUp Controller", () => {
-  test("Should return 400 if no name is provided", () => {
-    const sut = new SignUpController();
-    const httpRequest = {
-      body: {
-        email: "any_email@mail.com",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
-    const httpRespone = sut.handle(httpRequest);
-    expect(httpRespone.statusCode).toBe(400);
-    expect(httpRespone.body).toEqual(new MissingParamError("name"));
-  });
-  test("Should return 400 if no email is provided", () => {
-    const sut = new SignUpController();
-    const httpRequest = {
-      body: {
-        name: "any_name",
-        password: "any_password",
-        passwordConfirmation: "any_password",
-      },
-    };
-    const httpRespone = sut.handle(httpRequest);
-    expect(httpRespone.statusCode).toBe(400);
-    expect(httpRespone.body).toEqual(new MissingParamError("email"));
-  });
+  const sut = new SignUpController();
+
+  const testMissingParam = (paramName: string) => {
+    test(`Should return 400 if no ${paramName} is provided`, () => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const httpRequest: { body: Record<string, any> } = {
+        body: {
+          name: "any_name",
+          email: "any_email@mail.com",
+          password: "any_password",
+          passwordConfirmation: "any_password",
+        },
+      };
+
+      httpRequest.body[paramName] = undefined;
+
+      const httpResponse = sut.handle(httpRequest);
+      expect(httpResponse.statusCode).toBe(400);
+      expect(httpResponse.body).toEqual(new MissingParamError(paramName));
+    });
+  };
+  testMissingParam("name");
+  testMissingParam("email");
+  testMissingParam("password");
+  testMissingParam("passwordConfirmation");
 });
